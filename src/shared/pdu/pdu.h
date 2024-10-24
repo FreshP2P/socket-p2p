3aa54ab1ee9ed9aa8d42d4dc57760c26e8382f02
Caleb Lam <caleb.lam@hotmail.com>
2024-10-24 17:55:41 -0400

    Add PDU types

--- src/shared/pdu/pdu.h
+++ src/shared/pdu/pdu.h
@@ -1,8 +1,11 @@
 #ifndef _P2P_PDU
 #define _P2P_PDU
 
+#include <netinet/in.h>
+
 #define STANDARD_BODY_SIZE 100
 #define DATA_BODY_SIZE 1024
+#define MAX_CONTENT_LISTING_LENGTH 5
 
 enum PDUType
 {
@@ -16,22 +19,83 @@ enum PDUType
   PDU_ERROR = 'E'
 };
 
+typedef struct PeerContentInfo
+{
+  char peer_name[10];
+  char content_name[10];
+} peer_content_info_t;
+
+struct PDUContentRegistrationBody
+{
+  peer_content_info_t info;
+  struct sockaddr_in address;
+};
+
+struct PDUContentDownloadRequestBody
+{
+  peer_content_info_t info;
+};
+
+struct PDUContentDeregistrationBody
+{
+  peer_content_info_t info;
+};
+
+struct PDUContentDataBody
+{
+  size_t data_len;
+  char data[DATA_BODY_SIZE];
+};
+
+struct PDUContentListingBody
+{
+  int total_pages;
+  int page_num;
+  char registered_contents[MAX_CONTENT_LISTING_LENGTH][10];
+};
+
+struct PDUAcknowledgement
+{
+};
+
+struct PDUErrorBody
+{
+  char message[STANDARD_BODY_SIZE];
+};
+
+union PDUBody
+{
+  // R type body
+  struct PDUContentRegistrationBody content_registration;
+  
+  // D/S type body
+  struct PDUContentDownloadRequestBody content_download_req;
+
+  // T type body
+  struct PDUContentDeregistrationBody deregistration;
+
+  // C type body
+  struct PDUContentDataBody content_data;
+  
+  // O type body
+  struct PDUContentListingBody content_listing;
+
+  // A type body
+  struct PDUAcknowledgement ack;
+
+  // E type body
+  struct PDUErrorBody error;
+};
+
 struct PDU
 {
   enum PDUType type;
-  
-  union
-  {
-    // TODO: As we develop the app, we will add the proper
-    // contents per PDU type. Note that we should always check the type
-    // before assuming the field that represents the body is accessed.
-    // Some examples are shown below.
-    
-    // D: Bytes of parts of a file
-    char data[DATA_BODY_SIZE];
-    
-    // E: Message of the error
-    char error_message[STANDARD_BODY_SIZE];
-  } body;
+
+  // TODO: As we develop the app, we will add the proper
+  // contents per PDU type. Note that we should always check the type
+  // before assuming the field that represents the body is accessed.
+  // Some examples are shown below.
+  union PDUBody body;
 };
+
 #endif
